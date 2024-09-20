9c0ccf52020ebb04815f4feb183de8814a8a327d
Jakub Pionke <jakubpionke670@gmail.com>
2024-09-20 09:28:13 +0200

    struktura strony

---
+++ biuro.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8" 
+    <title>Biuro podróży</title>
+    <link rel="stylesheet" href="styl9.css" />
+</head>
+    <header>
+        <h1>Biuro podróży</h1>
+    </header>
+    <ul>
+        <li><a href=>Promocje</a></li>
+        <li><a href=>W tym roku jedziemy do...</a></li>
+        <li><a href=> Kontakt</a></li>
+    </ul>
+    <>
+        <section>
+            <h2>Promocje</h2>
+            <table="1">
+                <tr>
+                    <th>Miasto</th>
+                </tr>
+                <tr>
+                    <th>Cena</th>
+                </tr>
+                <>
+                    <td>Warszawa</td>
+                    <td> od 600 zł</td>
+                <tr>
+                    <td>Wenecja</td>
+                    <td>od 1200 zł</td>
+                </tr>
+                <tr>
+                    <td>Paryż</td>
+                    <td>od 1200 zł</td>
+                </tr>    
+                </table>
+        </section>    
+        <section>
+        <h2>Oferty</h2>
+        <p>W tym roku pojedziemy do...</p>
+        </section
+         <section>
+            <h2>Kontakt</h2>
+            <p>Napisz do nas: <a href="biuro@wycieczki.pl">biuro@wycieczki.pl</a>napisz do nas!</p>
+            <p>telefon: 444555666</p>
+         </section>
+         <section>
+            <h3>W poprzednich latach byliśmy...</h3>
+            <p>Strone wykonał: Karol Kort 4Ti</p>
+         </section>
+</main>
+  </body>
+</html>
